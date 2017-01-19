0b010bb712f02c87abcc529d9e274e8e8de5abc3
wko <wing.hong.a.ko@gmail.com>
2017-01-19 00:05:29 -0800

    smooooooth

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
-var stop = false;
-var windowSize;
 var defaultLineWidth = window.devicePixelRatio <= 1 ? 2 : 1;
+var windowSize;
 
+/** Each iteration, we draw a full arc */
 var iter = 0;
+/** Each end iteration, we reach the edge of the canvas and traverse back to the center */
 var endIter = 0;
+/** Length of an arc to draw per tick (speed of animation) */
+var lengthPerTick = 10;
+/** Refresh interval in ms */
+var refreshInterval = 30;
 
 var primaryColors = ["000000"];
 var primaryColors = ["031DFF", "0AA4FF", "03D0E8", "03FFD4"];
@@ -16,7 +21,7 @@ var mirrorEnabled = true;
 var maxIters = 50;
 var maxEndIters = 4;
 
-var debug = true;
+var debug = false;
 var storeRandom = [];
 var debugWindowSize = 679;
 var debugRandom = [26.940239797108266,3.082881315349646,39.908285498437166,2.5616484469781677,27.000734163945992,1.8984444033437886,49.92217708035817,2.229463822493238,21.029851301634526,0.010437991815959655,22.44315941658912,2.382030670093369,31.385171969721526,1.0097977465863788,23.89710731513463,1.37210017579784,46.404332769512365,2.1420127061377667,42.05320099949865,0.6926920764888791,40.79656562709418,0.4271523548731313,42.2436978190472,1.2035645803568855,38.65259568013245,2.9426243938092767,24.7834154928548,2.7833627024822225,24.58675018396172,0.14570950607774896,23.299190997236362,2.5034707881814176,48.024258916169096,2.2297729998583407,36.31394277455227,2.3466018104543345,36.15671393501213,0.6906027783363308,42.31523537698024,1.6845550946718875,36.60672643453391,1.2384650187944668,40.197120415118576,1.3022272731468434,37.06708528021163,0.3730572895964945,35.6922713585612,0.2985769772173546,36.297917278872134,1.0394294113750677,36.93140779955076,0.7989099398881074,27.56042274570799,0.03207989974658804,24.369778534443647,0.5095025735006591,22.663566533507804,0.7882192435917591,46.265977923920204,0.6311659378155667,24.25858550758324,0.8380615074834942,28.906898549617978,1.0103768935744495,46.01805293865753,2.1562849484595916,44.003042086594775,2.7160686824990306,28.389735366213618,1.8280232136570744,42.759506947321995,0.7312088449022315,45.39418080748936,1.4145300482945493,38.77387896753385,0.548141227184111,44.099554133223364,2.717879600079727,25.992187726617097,2.55505118436093,35.09239632631704,0.5751957900506862,26.449488145384585,1.6626762053692588,30.277341733748095,1.9770386812062246,32.67250008654892,1.316675101953378,47.07871348625002,1.0692950361375473,47.14242980331655,0.7587027664616546,34.282363759226435,1.6472881571392033,22.223028726169765,0.34451849585998,36.49029439558298,1.385569271285071,45.97020991049736,0.5519270162160492];
@@ -39,10 +44,12 @@ function gaussian(mu, sigma) {
 
 /** @return Randomly chosen value from uniform distribution on [0, 1) */
 function uniform(lower, upper) {
+  var r;
   if (debug) {
-      return debugRandom.shift();
+    r = debugRandom.shift();
+  } else {
+    r = lower + Math.random() * (upper - lower);
   }
-  var r = lower + Math.random() * (upper - lower);
   storeRandom.push(r);
   return r;
 }
@@ -225,8 +232,8 @@ function circleAtTarget(from, to, angle) {
 	 y: this.center.y + this.radius * Math.sin(this.angle + this.deltaAngle)
        };
      },
-     /** Updates state */
-     update: function() {       
+     /** Chooses a new arc */
+     chooseNewArc: function() {       
        var current = this.end();
        var angle = (this.angle + this.deltaAngle + Math.PI) % (2 * Math.PI);
        var radius = uniform(20, 50);
@@ -258,6 +265,8 @@ function circleAtTarget(from, to, angle) {
 	 } else {
 	   deltaAngle = - Math.acos(-center.x / radius) - angle;
 	 }
+	 
+	 endIter++;
        } else if (this.center == null) {
 	 center = {
 	   x: -radius * Math.cos(currentState.angle),
@@ -274,6 +283,7 @@ function circleAtTarget(from, to, angle) {
        this.radius = radius;
        this.angle = angle;
        this.deltaAngle = deltaAngle;
+       this.subArc = lengthPerTick / (radius * Math.abs(deltaAngle));
      },
      arcLength: function() {
        return 2 * this.radius * deltaAngle;
@@ -285,20 +295,37 @@ function circleAtTarget(from, to, angle) {
      // starting angle of the arc
      angle: 0,
      // change in angle of the arc
-     deltaAngle: -Math.PI
+     deltaAngle: -Math.PI,
+     // percentage of arc we draw in each tick
+     subArc: 0,
+     // current tick of the current arc
+     tick: 0
    };
    
    // draw a single iteration
    var draw = function() {
-     currentState.update();
+     if (currentState.subArc === 0 || currentState.tick * currentState.subArc >= 1) {
+       currentState.chooseNewArc();
+       currentState.tick = 0;
+     }
+     
+     var startAngle = currentState.angle + currentState.tick * (currentState.deltaAngle * currentState.subArc);
+     var deltaAngle = currentState.subArc * currentState.deltaAngle;
+     if ((currentState.tick + 1) * currentState.subArc >= 1) {
+       deltaAngle = (1 - currentState.tick * currentState.subArc) * currentState.deltaAngle;
+       // ok this is kind of sneaky ...
+       iter++;
+     }
+     
+     currentState.tick++;
      
      if (mirrorEnabled) {
        ctx.strokeStyle = '#A9A9A9';
        drawArcs(
 	 createMirror(toPolar(currentState.center)),
 	 currentState.radius,
-	 -currentState.angle,
-	 -currentState.deltaAngle
+	 -startAngle,
+	 -deltaAngle
        );
      }
 
@@ -306,8 +333,8 @@ function circleAtTarget(from, to, angle) {
      drawArcs(
        toPolar(currentState.center),
        currentState.radius,
-       currentState.angle,
-       currentState.deltaAngle
+       startAngle,
+       deltaAngle
      );
      
      if (debug) {
@@ -317,15 +344,9 @@ function circleAtTarget(from, to, angle) {
    
    var timedDraw = function() {
      draw();
-     
-     iter ++;
-     var end = currentState.end();
-     if (end.x * end.x + end.y * end.y < 0.1) {
-       endIter ++;
-     }
-     
+          
      if (iter < maxIters && endIter < maxEndIters) {
-       setTimeout(timedDraw, 100);
+       setTimeout(timedDraw, refreshInterval);
      } else {
        console.log("window size: " + windowSize);
        console.log("random: " + JSON.stringify(storeRandom));
